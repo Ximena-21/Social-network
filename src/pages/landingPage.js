2c1320b934462383662680451aead8e991a5560d
Ximena-21 <ximenanzola.21@gmail.com>
2022-10-04 21:28:30 -0400

    arreglo estilos y resp[onsive de landingPAge

--- src/pages/landingPage.js
+++ src/pages/landingPage.js
@@ -6,26 +6,18 @@ function createLandingPage () {
     landingPageContainer.className = "landingPage"
 
     landingPageContainer.innerHTML = `
-    <header class='landingPage_header'>
-        <nav class=" landingPage_header--nav nav">
-            <div class="nav_menuLogo">
-                <img src="../img/K__1_-removebg-preview 2.png" alt="" class="nav_menuLogo--img"/>
-                <button class="nav_menuLogo--abrirMenu abrirMenu menuAbierto" aria-label="Abrir menu">
-                    <img src="../img/Group 6.png" alt="abrir menu">
-                </button>
-            </div>
-            <ul class="nav_menu menu">
-                <button class="menu_cerrarMenu cerrarMenu" aria-label="Cerrar menu">
-                    <img height="33" width="33" src="../img/cerrar_menu.png" alt="cerrar menu">
-                </button>
-                <li class="menu_li"><a href="#inicio">Inicio</a></li>
-                <li><a href="#ventajas">Ventajas</a></li>
-                <li><a href="#quienesSomos">Quienes Somos</a></li>
-                <li><a href="#Contacto">Contacto</a></li>
-            </ul>
-            <button class='singIn nav_sing'>Iniciar sesión</button>
-        </nav>
+
+    <header class="landingPage_header header">
+
+        <div class="header_top">
+            <img class="header_top--openMenu" src="../img/menu.png" alt="openMenu">
+            <button class='header_top--singIn' id="btn_singIn">Iniciar sesión</button>
+            <img class="header_top--logo" src="../img/logoKnitters.png" alt="logo" >
+        </div>
+
+            
     </header>
+
     <section id="inicio" class="landingPage_main">
         <div class="landingPage_main--welcomeBox welcomeBox">
             <div class="welcomeBox_container">
@@ -48,7 +40,7 @@ function createLandingPage () {
                     <h2 class="advantage_section--h2">comparte</h2>
                 </div>                                  
             </div>
-            <div class="adventage_container">   
+            <div class="advantage_container">   
                 <div class="advantage_section">
                     <img src="../img/clasificacion.png" alt="" class="advantage_section--img">
                     <h2 class="advantage_section--h2">Haz Comunidad</h2>
@@ -71,13 +63,13 @@ function createLandingPage () {
     </section>
     <footer class="landingPage_footer footer">
             <div class="footer_redes">
-                <div class="footer_logo">
+                <div class="footer_logoRedes">
                     <img src="../img/FacebookLogo.png" alt="" class="footer_img">
                     <img src="../img/InstagramLogo.png" alt="" class="footer_img">
                     <img src="../img/TwitterLogo.png" alt="" class="footer_img">
                 </div>
                 <div class="footer_logo">
-                    <img src="../img/K__1_-removebg-preview 2.png" alt="" class="footer_img--logo">
+                    <img src="../img/logoK.png" alt="" class="footer_img--logo">
                 </div>
             </div>
             <div class="footer_autor">
@@ -85,16 +77,16 @@ function createLandingPage () {
             </div>
     </footer>
     `
-    console.log(landingPageContainer.querySelector('.singIn'))
+    // console.log(landingPageContainer.querySelector('.singIn'))
 
-    landingPageContainer.querySelector('.singIn').addEventListener('click', () => {
-        // console.log('sirve el boton iniciar sesion')
-        onNavigate('/login')
-    })
-    landingPageContainer.querySelector('.register').addEventListener('click', () => {
-        // console.log('sirve el boton iniciar sesion')
-        onNavigate('/register')
-    })
+    // landingPageContainer.querySelector('.singIn').addEventListener('click', () => {
+    //     // console.log('sirve el boton iniciar sesion')
+    //     onNavigate('/login')
+    // })
+    // landingPageContainer.querySelector('.register').addEventListener('click', () => {
+    //     // console.log('sirve el boton iniciar sesion')
+    //     onNavigate('/register')
+    // })
     return landingPageContainer
 }
 
@@ -116,3 +108,17 @@ function toggleMenu() {
 // //evento a la funcion o ejecutar
 // abrir_menu_b.("click",toggleMenu);
 // cerrar_menu_b.addEventListener("click",toggleMenu);
+
+
+//header
+
+/* <nav class="landingPage_header--menuNav menuNav landingPage_menuHidden">
+<ul class="menuNav_list">
+    <img class="menuNav_btnCloseMenu" src="" alt="closeMenu">
+   
+    <li><a class="menuNav_item" href="" id="home">Inicio</a></li>
+    <li><a class="menuNav_item" href="" id="advantage">Ventajas</a></li>
+    <li><a class="menuNav_item" href="" id="aboutUs">Quienes Somos</a></li>
+    <li><a class="menuNav_item" href="" id="contact">Contacto</a></li>
+</ul>
+</nav> */
