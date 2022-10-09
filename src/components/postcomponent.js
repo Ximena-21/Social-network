e1052862d79b32dc35b2d0f82ef24791367ba21c
Ximena-21 <ximenanzola.21@gmail.com>
2022-10-09 15:23:06 -0500

    nuevo diseño de posts

--- src/components/postcomponent.js
+++ src/components/postcomponent.js
@@ -1,23 +1,49 @@
-import { dataPost } from "./dataPost.js"
 
 const Post = (userPost) => {
     const postContainer = document.createElement('div')
     postContainer.className = 'postContainer'
 
+
+    console.log(userPost)
+
+    const likes = userPost.likes?.length || 0 
+
     postContainer.innerHTML = `
+
         <div class="postContainer_headerPost">
             <div class="postContainer_userBox">
                 <img src="../img/avatar.png" alt="" class="postContainer_user">
-                <span class="postContainer_userName">${userPost.emailUser}</span>
-            </div>
-            <div class="postContainer_interactionBox">
-                <img src="../img/commet.png" alt="comment" class="postContainer_icon" id= "comment">
-                <img src="../img/like.png" alt="like" class="postContainer_icon" id="like">
+                <span class="postContainer_userName">${userPost.nameUser}</span>
             </div>
         </div>
+
+        <div>
+            ${userPost.description || ''}
+        </div>
+            
         <div class="postContainer_postImage">
             <img src="${userPost.image}" alt="imgPost" class="postContainer_postImage--img">
         </div>
+
+        <div class="postContainer_interactionBox">
+
+            <div>
+                <img src="../img/commet.png" alt="comment" class="postContainer_icon" id= "comment">
+                <input type='text' placeholder='comentario'/>
+            </div>
+
+            <div>
+                <img src="../img/like.png" alt="like" class="postContainer_icon" id="like">
+                <span>${likes}</span>
+            </div>
+
+        </div>
+
+        
+        <div >
+            <p>comentarios</p>
+        </div>
+            
     `
 
     return postContainer
