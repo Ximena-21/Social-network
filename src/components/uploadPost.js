787fc14845e7deefa530fb7126c1808a317d0e23
Ximena-21 <ximenanzola.21@gmail.com>
2022-10-09 14:11:15 -0400

    ultimos cambios

--- src/components/uploadPost.js
+++ src/components/uploadPost.js
@@ -3,28 +3,27 @@ function uploadPost() {
     uploadPostContainer.className = 'uploadPost'
 
     uploadPostContainer.innerHTML = `
-        <div class="uploadPost_container">
+        <form action="" method="post" class="uploadPost_container">
             <button class="uploadPost_btnClosed" id="btnClose">x</button>
             <input type="file" name="postImg" id="postImg" class="uploadPost_img">
+            <div  class="uploadPost_previewContainer" id="previewContainer" >
+                <img src="../img/more.png" id="preview" class="uploadPost_preview"/>
+            </div>
             <input type="text" name="postText" id="postText" class="uploadPost_text">
-            <button class="uploadPost_btn" id="btnPost">Subir Post</button>
-        </div>
+            <button class="uploadPost_btn" id="btnPost">Subir Post</button>      
+        </form>
     `
-<<<<<<< HEAD
-    
-=======
 
-   uploadPostContainer.querySelector('#postImg').onchange = (event)=>{
+    uploadPostContainer.querySelector('#postImg').onchange = (event) => {
         console.log('elevento que entra', event)
-        const fr  = new FileReader();
+        const fr = new FileReader();
         fr.readAsDataURL(event.target.files[0]);
-        fr.onload = function(){
-            const image =uploadPostContainer.querySelector('#preview');
+        fr.onload = function () {
+            const image = uploadPostContainer.querySelector('#preview');
             image.src = fr.result;
         };
-   }
- 
->>>>>>> 65cbe9403bffd1fbdcdc59e13a157637fbfe55d8
+    }
+
     return uploadPostContainer
 }
 
